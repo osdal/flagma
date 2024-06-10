387ac4282ee7b81a3ebcf4bdaeb4946d50735251
Дмитрий <osdal67@gmail.com>
2024-06-10 22:31:12 +0300

    o

--- flagma.py
+++ flagma.py
@@ -12,13 +12,28 @@ html = response.text
 response.encoding = 'utf-8'
 soup = bs(html, "html.parser")
 
-data_ad = {}
+links = []
 data_ads = soup.findAll('div', 'header job')
 for name_ad in data_ads:
     job_name = name_ad.find('h2').text
 
 for limk_ad in data_ads:
     link = limk_ad.find('a')['href']
+    links.append(link)
+
+for link in links:
+    response = requests.get(link)
+    html = response.text
+    soup = bs(html, "html.parser")
+    title = soup.find('h1').text
+    description = soup.find(id='description-text').text
+    # print(description)
+    data_ad = {
+        'title': title,
+        'description': description,
+        'view_url': link
+    }
+    print(data_ad)
 
 for i in range(len(data_ads)):
     data_ad = {
@@ -26,5 +41,4 @@ for i in range(len(data_ads)):
         'link': link,
     }
     data_ads[i] = data_ad
-print(data_ads)
 # print(len(data_ads))
